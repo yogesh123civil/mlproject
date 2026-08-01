af566112a7e68f2f55dacc5a460d076e12cb01ef
Yogesh Kumawat <yogeshkumawat@gmail.com>
2026-08-01 16:03:08 +0530

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,43 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifact','train.csv')
+    test_data_path: str=os.path.join('artifact','test.csv')
+    raw_data_path: str=os.path.join('artifact','data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+    
+    def initiate_data_ingestion(self):
+        logging.info("entered the data ingestion method or component")
+        
+        try:
+            df=pd.read_csv("notebook/data/stud.csv")
+            logging.info('Read the dataset as dataframe')
+            os.makedirs(self.ingestion_config.train_data_path, exist_ok=True)
+            df.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            logging.info('Train data saved successfully')
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            
+            logging.info('Test data saved successfully')
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            
+            raise CustomException(e, sys)
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
--- src/exception.py
+++ src/exception.py
@@ -16,7 +16,7 @@ def error_message_detail(error, error_detail: sys):
     return error_message
 
 
-class customException(Exception):
+class CustomException(Exception):
 
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
